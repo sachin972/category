24565f41522802cae8e6756396cdf6706a325d8a
sachin972 <sachintripathi972000@gmail.com>
2024-07-31 13:25:58 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Category from "./Category";
 
 function App() {
   return (
